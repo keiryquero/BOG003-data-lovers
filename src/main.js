6f8f79f9aeab50b1f2e02155462a9fce8cc2e7cb
keiry <keiryquero@gmail.com>
2021-08-05 11:11:48 -0500

    prubas iniciales

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+import films from './data/ghibli/ghibli.js';
+
+//import data from './data/pokemon/pokemon.js';
+// import data from './data/rickandmorty/rickandmorty.js';
+console.log(example);
+console.log(films);
