2e168e424d0f78682bec7eead328e367a189aba9
keiry <keiryquero@gmail.com>
2021-08-12 10:37:45 -0500

    maquetacion 3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
+
+
 import { example } from './data.js';
 
 import films from './data/ghibli/ghibli.js';
 
-console.log(example);
-console.log(films);
+/*console.log(example);
+console.log(films);*/
+
+document.getElementById("siguiente").addEventListener("click", () => {
+    document.getElementById("cabecera").style.display="none";
+     document.getElementById("vista2").style.display = "block";
+   })
