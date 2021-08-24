875b0a612b5c35d3c024554c28a222fa55dbd230
keiry <keiryquero@gmail.com>
2021-08-24 11:52:29 -0500

    actualizacion masiva de archivos 24-08-21

--- src/data.js
+++ src/data.js
@@ -1,12 +1,29 @@
-// estas funciones son de ejemplo
-
-
-
-export const example = () => {
-  return 'hola';
-
+// Funcion filter, para filtrar las peliculas menores al año 2000
+export function filtroPeliculas(films){ 
+  let mayorMenor= films.filter((films) => films.release_date < 2000 );
+  //console.log(mayorMenor)
+  return mayorMenor;
 };
+//Prueba del arrow function = funcion funcion flecha
+  export const ordenarAño = (films) => {
+  return films.sort((year1,year2) => year2.release_date - year1.release_date 
+  )};
+// funcion Reduce,muestra promedio de las peliculas famosas > 90 puntos
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+//Funcion Sort, para ordenar de mayor a menor
+/*export function ordenarAño(films){
+  let ordenar =  films.sort((año1,año2) => {
+    return año2.release_date - año1.release_date
+  });
+   //console.log(ordenar);
+  return ordenar;
+
+};*/
+
+
+
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,53 +1,90 @@
+import { filtroPeliculas } from './data.js';
+import { ordenarAño } from './data.js';
+//import { reduceData } from './data.js';
+import studio from './data/ghibli/ghibli.js';
+let films = studio.films;
 
+function tarjeta1(films) {
+//1. recorre el array de Films 
+  document.getElementById("container").innerHTML= "";
+  const contenedorTarjeta = document.createElement("div");
 
-import { example } from './data.js';
+  for (var i = 0; i < films.length; i++) {
+    let title = films[i].title;
+    let year = films[i].release_date;
+    let poster = films[i].poster;
+    let points= films [i].rt_score;
 
-import { ghibli } from './data.js';
+    // Se crean elementos que van dentro del contenedorTarjeta
 
-import data from './data/ghibli/ghibli.js';
+    const tarjeta = document.createElement("div");
+    const imagen = document.createElement("img");
+    const titulo = document.createElement("h3");
+    const año = document.createElement("h4");
+    const puntos = document.createElement("p");
+   
+    imagen.src = poster;
+    titulo.textContent = title;
+    año.textContent = year;
+    puntos.textContent = "Puntuación: " + points;
+    
+    tarjeta.appendChild(imagen);
+    tarjeta.appendChild(titulo);
+    tarjeta.appendChild(año);
+    tarjeta.appendChild(puntos);
+    contenedorTarjeta.appendChild(tarjeta);
+    //console.log(tarjeta, titulo, año, imagen);
+    
+  }
+  return contenedorTarjeta;
+};
 
-console.log(ghibli);
-console.log(data);
+// Invoca la funcion tarjeta1
+document.getElementById("link2").addEventListener("click", () => {
+document.getElementById("container").appendChild(tarjeta1(films));
+});
+//invoca la funcion filter
+const filtrarPeliculas = filtroPeliculas(films);
+document.getElementById("filtro").addEventListener("click", () => {
+document.getElementById("container").appendChild(tarjeta1(filtrarPeliculas));
+});
+//invoca funcion Sort
+let ordenar= ordenarAño(films);
+tarjeta1(ordenar);
 
+//invoca funcion Reduce
+document.getElementById("link3").addEventListener("click", () => {
+document.getElementById("ranking").appendChild(tarjeta1(films));
+});
 
-const menuIcon = document.querySelector(".menu");
-const menu = document.querySelector(".menu-despegable");
-let openMenuBtn = document.querySelector(".open-menu");
-let closeMenuBtn = document.querySelector(".close-menu");
+//Permite llamar la Vista de Catologo
+document.getElementById("link2").addEventListener("click", () => {
+document.getElementById("cabecera").style.display = "none"
+document.getElementById("catalogo").style.display = "block"
+document.getElementById("topPeliculas").style.display = "none"
+});
+//Retorna a la pagina Principal
 
-console.log(menuIcon);
-console.log(menu);
+document.getElementById("link1").addEventListener("click", () => {
+  document.getElementById("cabecera").style.display = "block"
+  document.getElementById("catalogo").style.display = "none"
+  document.getElementById("topPeliculas").style.display = "none"
+});
+//Permite llamar la vista topPeliculas
+  document.getElementById("link3").addEventListener("click", () => {
+  document.getElementById("cabecera").style.display = "none"
+  document.getElementById("catalogo").style.display = "none"
+  document.getElementById("topPeliculas").style.display = "block"
 
-menuIcon.addEventListener("click", ()=>{
-    menu.classList.toggle("spread")
-})
+});
 
-openMenuBtn.addEventListener("click",menuIcon);
-closeMenuBtn.addEventListener("click",menuIcon);
-
-
-
-/*window.addEventListener("click", e=>{
-    if(menu.classList.contains("spread")
-    && e.target != menu  && e.target != menuIcon)
-    {
-        menu.classList.toggle("spread")
-    }
-})*/
-
-/*let openMenuBtn = document.querySelector(".open-menu");
-let closeMenuBtn = document.querySelector(".close-menu");
-
-function toggleMenu(){
-    menu.classList.toggle("menu_opened");
-}
-openMenuBtn.addEventListener("click",toggleMenu);
-closeMenuBtn.addEventListener("click",toggleMenu);*/
-
-/*console.log(example);
-console.log(films);*/
-
-document.getElementById("siguiente").addEventListener("click", () => {
-    document.getElementById("cabecera").style.display="none";
-     document.getElementById("vista2").style.display = "block";
-   })
+/* Toma el elemento container y lo sustituye por nuevos elementos, acumulando 
+    los elementos para mostrarlo en el documento */
+    //document.getElementById("container").innerHTML= "";
+    /*document.getElementById("container").innerHTML +=`
+    <div>
+    <img src = ${poster}>
+    <h3>${title}</h3>
+    <p>${year}</p>
+    </div>`*/
+    //a los elemntos se le está asignando las variables
